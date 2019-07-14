a4f862d1a2d2f2de418d713dc15a9bb46a47b88c
Caio Guedes <caioguedesam@gmail.com>
2019-07-14 19:43:52 -0300

    First commit, FPS camera and movement implemented

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script controls a first person camera's rotation using mouse axis
+// Still has to be extended for joystick controls, using right analog stick
+// Heavily inspired by this tutorial: https://www.youtube.com/watch?v=n-KX8AeGK7E
+
+// Caio Guedes de Azevedo Mota, 07/2019
+
+public class PlayerLook : MonoBehaviour
+{
+    // Input variables
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [Range(1f, 100f)]
+    [SerializeField] private float mouseSensitivity;
+    // Clamp variable to control adequate clamping
+    private float xAxisClamp;
+
+    // Player body reference
+    [SerializeField] private Transform playerBody;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    // Locks cursor in center by simply setting cursor lock state to locked
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Controls X axis camera rotation (Up/Down).
+    // Y axis rotation (Left/Right) is done by using forward vector
+    // in player movement rather than camera movement, so that the player's
+    // body rotates along the camera.
+    private void CameraRotation()
+    {
+        // Getting input
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity/5f;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity/5f;
+
+        // Controlling clamp by limiting rotation, xAxisClamp stores amount rotated
+        xAxisClamp += mouseY;
+        // If the total rotation exceeds upper bound (90 deg),
+        // set the rotation input to 0 so it won't rotate
+        // and call Clamp function to avoid buggy behavior
+        if(xAxisClamp > 90f)
+        {
+            xAxisClamp = 90f;
+            mouseY = 0.0f;
+            ClampXAxisRotation(270f);
+        }
+        // Same as above but about lower bound (-90 deg)
+        else if(xAxisClamp < -90f)
+        {
+            xAxisClamp = -90f;
+            mouseY = 0.0f;
+            ClampXAxisRotation(90f);
+        }
+
+        // Rotate based on input
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    // Directly clamps x axis rotation to a specific value
+    // This stops camera from overshooting the determined clamp
+    // by altering euler angles directly.
+    private void ClampXAxisRotation(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    // Movement variables
+    [SerializeField] private float moveSpeed;
+    private bool isJumping;
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 forwardMove = transform.forward * verticalInput;
+        Vector3 sideMove = transform.right * horizontalInput;
+
+        controller.SimpleMove(Vector3.ClampMagnitude(forwardMove + sideMove, 1.0f) * moveSpeed);
+
+        JumpInput();
+    }
+
+    private void JumpInput()
+    {
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        controller.slopeLimit = 90f;
+        float timeInAir = 0.0f;
+
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            controller.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (!controller.isGrounded && controller.collisionFlags != CollisionFlags.Above);
+
+        controller.slopeLimit = 45f;
+        isJumping = false;
+    }
+}
