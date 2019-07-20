e603f7ff1c57ad66b6b21f29f013dc26ab5e4149
Caio Guedes <caioguedesam@gmail.com>
2019-07-20 15:37:32 -0300

    Portals working. Still needs smoothing

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // Player Character controller reference
+    public CharacterController controller;
+
+    // Opposing portal reference
+    public GameObject opposingPortal;
+    public bool portalOn = false;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag != "Level" && opposingPortal.GetComponent<Portal>().portalOn == true) {
+            controller.enabled = false;
+
+            // This only moves portaled player to a position forward of the opposing portal
+            // and makes it look forward to the portal.
+            // Still have to do smooth movement (like in the actual game)
+            controller.transform.position = opposingPortal.transform.position + opposingPortal.transform.forward * 2;
+            controller.transform.rotation = Quaternion.LookRotation(opposingPortal.transform.forward);
+
+            controller.enabled = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/PortalGun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGun : MonoBehaviour
+{
+    // Player camera reference
+    public Camera cam;
+
+    // Left/Right portal references
+    public GameObject leftPortal;
+    public GameObject rightPortal;
+
+    private void Update() {
+        
+        if(Input.GetButtonDown("Fire1"))
+            ThrowPortal(leftPortal);
+        else if(Input.GetButtonDown("Fire2"))
+            ThrowPortal(rightPortal);
+    }
+
+    private void ThrowPortal(GameObject portal) {
+
+        portal.GetComponent<Portal>().portalOn = true;
+        RaycastHit hit;
+        LayerMask portalable = 1 << 8;
+
+        // Casts a ray on main camera center that checks if the object hit is valid to portal
+        // (at the moment just the player isn't valid) and throws the portal
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, portalable)) {
+            if(hit.transform.tag != "Player") {
+                portal.transform.position = hit.point;
+                portal.transform.rotation = Quaternion.LookRotation(hit.normal);
+            }
+        }
+    }
+}
